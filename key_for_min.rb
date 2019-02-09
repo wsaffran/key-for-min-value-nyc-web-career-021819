8e686c9408ec4dcd2ee54979e884261dcc550f9b
Will Saffran <willh.saffran@gmail.com>
2019-02-09 19:11:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,14 +11,4 @@ def key_for_min_value(name_hash)
     end 
   end 
   lowest_key
-
-  
-  
-  #lowest_key = nil
-  #name_hash.each do |key, value|
-    #if key < lowest_key
-      #lowest_key = key
-    #end
-  #end
-  #lowest_key
 end
