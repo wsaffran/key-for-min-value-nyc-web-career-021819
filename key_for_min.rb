7a5e5a3eeee837cf01d9e05c2b280de1e3be9ff6
Will Saffran <willh.saffran@gmail.com>
2019-02-09 19:10:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@
 def key_for_min_value(name_hash)
   lowest_key = nil 
   lowest_value = nil 
-  name_hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
+  name_hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
       lowest_value = v 
       lowest_key = k 
     end 
